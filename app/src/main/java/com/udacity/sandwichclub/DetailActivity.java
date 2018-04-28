0c2fffbd07384c063a7e15efc6a1c2659ac51f9c
loftywaif002 <deep.row4@gmail.com>
2018-04-27 18:16:51 -0700

    parsing json test

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -11,6 +12,8 @@ import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = DetailActivity.class.getName();
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -7,19 +7,42 @@ import com.udacity.sandwichclub.model.Sandwich;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     private static final String TAG = JsonUtils.class.getName();
 
     /*
     * Initialize JSONObject from JSON String
+    * Loop through the JSON object and get the
     *
     * */
     public static Sandwich parseSandwichJson(String json){
 
         try{
             JSONObject sandwich_details = new JSONObject(json);
-            Log.i(TAG,sandwich_details.toString());
+            JSONObject name = sandwich_details.getJSONObject("name");
+            /*
+            * To get the strings, optString method is used since we are not sure if the data will be there or not.
+            * If we use getString, it will throw an exception if the value is empty
+            * */
+            String mainName = name.optString("mainName");
+            String alsoKnownAs = name.optString("alsoKnownAs"); //This is an Array
+
+            String placeOfOrigin = sandwich_details.optString("placeOfOrigin");
+
+            String description = sandwich_details.optString("description");
+            String image_url = sandwich_details.optString("image");
+            String ingredients = sandwich_details.optString("ingredients"); //This is an array
+
+            Log.i(TAG,mainName);
+            Log.i(TAG,alsoKnownAs);
+            Log.i(TAG,placeOfOrigin);
+            Log.i(TAG,description);
+            Log.i(TAG,image_url);
+            Log.i(TAG,ingredients);
         }catch (JSONException e){
             Log.e(TAG, e.toString());
         }
