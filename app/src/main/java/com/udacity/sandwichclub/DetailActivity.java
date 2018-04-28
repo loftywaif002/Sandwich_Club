e8343e7f3636b4a6f8f0daf8fbdfb30198d2f1c3
loftywaif002 <deep.row4@gmail.com>
2018-04-28 03:35:37 -0700

    populating data to textviews

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     private static final String TAG = DetailActivity.class.getName();
+    private Sandwich sandwich;
+    private TextView also_known_label, also_known_as_textView, place_of_origin_label, place_of_origin_textView, sandwich_description, sandwich_ingredients;
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
@@ -24,6 +30,12 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        also_known_label = findViewById(R.id.alias_label_textView);
+        also_known_as_textView = findViewById(R.id.also_known_textView);
+
+        place_of_origin_label = findViewById(R.id.place_label_textView);
+        place_of_origin_textView = findViewById(R.id.place_of_origin_textView);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -39,7 +51,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         Log.i(TAG,sandwich.toString());
         if (sandwich == null) {
             // Sandwich data unavailable
@@ -60,7 +72,45 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
+    /*
+    * For Lists,Simply Converting the List to String is printing only the last value
+    * Since its replacing the old value with current value in each iteration inside for-loop
+    * So, we read from buffer and append with a Stringbuilder object to get all the String values and then show it
+    *
+    * If there is no data found, then the labels and textviews will be hidden from user
+    * */
     private void populateUI() {
+        /*
+        * READING FROM THE LISTS
+        * */
+        StringBuilder mStringBuilder = new StringBuilder();
+        if (sandwich.getAlsoKnownAs().isEmpty()){
+
+            also_known_label.setVisibility(View.GONE); //Hide the textview
+            also_known_as_textView.setVisibility(View.GONE);
+
+        }else{
+            String delm = "";
+            for (String str : sandwich.getAlsoKnownAs())
+                {
+                    mStringBuilder.append(delm);
+                    delm = ",";
+                    mStringBuilder.append(" ");
+                    mStringBuilder.append(str);
+                }
+            also_known_as_textView.setText(mStringBuilder);
+        }
+
+        if (sandwich.getPlaceOfOrigin().isEmpty()){
+
+            place_of_origin_label.setVisibility(View.GONE); //Hide the textview
+            place_of_origin_textView.setVisibility(View.GONE);
+
+        }else{
+            place_of_origin_textView.setText(sandwich.getPlaceOfOrigin());
+        }
+
 
     }
+
 }
